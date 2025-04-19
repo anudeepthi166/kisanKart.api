f8773f73a9682a6d7b15cca0239a97fc5b8dad18
anudeepthi166 <anudeepthikolagani.999@gmail.com>
2025-04-19 20:35:45 +0530

    Added order routes and controllers

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { addToCart, clearCart, deleteCartItem, getCart, updateCartItem} = require('../controllers/cartController')
-router.get('/:userId', getCart)
-router.post('/add', addToCart)
-router.put('/update/:itemId', updateCartItem)
-router.delete('/delete/cartItem/user/:userId/product/:productId', deleteCartItem)
+
+router.post('/', addToCart)
+router.get('/user/:userId', getCart)
+router.put('/itemId/:itemId', updateCartItem)
+router.delete('/cartItem/user/:userId/product/:productId', deleteCartItem)
 router.delete('/clear/user/:userId', clearCart)
 
 module.exports = router;
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const {addProduct, deleteProduct, getAllProducts, updateProduct, getProductById} = require("../controllers/productController");
 const upload = require("../utils/multer");
 
+router.post('/', upload.single('image'),addProduct);
 router.get('/', getAllProducts);
 router.get('/:productId', getProductById)
-router.post('/add', upload.single('image'),addProduct);
-router.put('/update/:id', updateProduct);
-router.delete('/delete/:id', deleteProduct);
+router.put('/:id', updateProduct);
+router.delete('/:id', deleteProduct);
 
 
 module.exports = router;
